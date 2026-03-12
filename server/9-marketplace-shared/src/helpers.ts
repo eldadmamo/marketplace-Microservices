b720502bdd5bf2e2c9e02edb549aaa48e2465e33
eldadmamo <eldadf456@gmail.com>
2026-03-12 13:44:32 +0300

    workflow updated

--- server/9-marketplace-shared/src/helpers.ts
+++ server/9-marketplace-shared/src/helpers.ts
@@ -28,3 +28,4 @@ export function isDataURL(value: string): boolean {
     /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\\/?%\s]*)\s*$/i;
     return dataUrlRegex.test(value);
 }
+    
